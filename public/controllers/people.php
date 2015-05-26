bc33634620d0f21da50bf0f3d5b9f4fd7f5380ce
Alan Stirling <alan@attopartners.com>
2015-05-26 16:46:32 +0100

    move people functionality to controller

---
+++ public/controllers/people.php
@@ -0,0 +1,15 @@
+<?php
+include 'models/person.php';
+
+$person = Person::find();
+
+if(!$person->id){
+  $new_person = new Person('Joe', 'Bloggs');
+  if ($new_person->save()){
+    $person = $new_person;
+  }
+  else{
+    echo '<br />Error saving person';
+  }
+} 
+?>
